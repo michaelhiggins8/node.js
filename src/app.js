20e7b160bb41a8248cf38953f4cc513da75f5074
michaelhiggins8 <150331335+michaelhiggins8@users.noreply.github.com>
2025-03-05 23:26:43 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ for(let i = 0; i<10;i++){
   console.log("d");
     console.log("d");console.log("d")
   console.log("d");console.log("d")
-  console.log("d")
+  console.log("d")console.log("a");
    console.log("b");
   console.log("c");
   console.log("d")
